a322a0bd0cf3fe26108c4ae41c6483ff7f8e49d7
chen <793658414@qq.com>
2023-02-20 11:44:15 +0800

    feat: init

---
+++ App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>12312312312313</Text>
+    </View>
+  );
+}
+
+export default App;
