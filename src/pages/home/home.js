15d67e9b487532414e2818b278ade956ff1c2582
chen <793658414@qq.com>
2023-02-20 18:26:58 +0800

    feat: eslint prettier

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,16 +1,16 @@
-import React, {Component} from 'react';
-import {Box, Button} from 'native-base';
-import {Flex, Center} from 'native-base';
+import React, {Component} from 'react'
+import {Box, Button} from 'native-base'
+import {Flex, Center} from 'native-base'
 
 class Home extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-    console.log(props);
+    console.log(props)
   }
 
   render() {
-    const {navigation} = this.props;
+    const {navigation} = this.props
 
     return (
       <Box bg={['red.400', 'blue.400']} flex={1}>
@@ -19,40 +19,16 @@ class Home extends Component {
             size="16"
             bg="primary.100"
             _text={{
-              color: 'coolGray.800',
+              color: 'coolGray.800'
             }}>
             100
-          </Center>
-          <Center
-            size="16"
-            bg="primary.200"
-            _text={{
-              color: 'coolGray.800',
-            }}>
-            200
-          </Center>
-          <Center
-            bg="primary.300"
-            size="16"
-            _text={{
-              color: 'coolGray.800',
-            }}>
-            300
-          </Center>
-          <Center
-            size="16"
-            bg="primary.400"
-            _text={{
-              color: 'coolGray.800',
-            }}>
-            400
           </Center>
         </Flex>
 
         <Button onPress={() => navigation.push('详情')}>Click Me</Button>
       </Box>
-    );
+    )
   }
 }
 
-export default Home;
+export default Home
