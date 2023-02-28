76ef5afbf2309a3ef5031a216e41c8f875a4f79e
chen <793658414@qq.com>
2023-02-28 17:05:53 +0800

    feat: 新增icons

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,63 +1,33 @@
 import React, {useState} from 'react'
-import {Box, Text, CheckIcon, HStack, Center, Pressable} from 'native-base'
+import {Box, Text, HStack, Center, Pressable} from 'native-base'
+import Icon from 'react-native-vector-icons/AntDesign'
 
 export default () => {
   const [selected, setSelected] = useState(1)
+
+  const tabbar = [
+    {name: '首页', icon: 'home'},
+    {name: '我的', icon: 'user'}
+  ]
+
   return (
     <Box width="100%">
-      <HStack bg="primary.600" alignItems="center" safeAreaBottom shadow={6}>
-        <Pressable
-          cursor="pointer"
-          opacity={selected === 0 ? 1 : 0.5}
-          py="3"
-          flex={1}
-          onPress={() => setSelected(0)}>
-          <Center>
-            <CheckIcon mb="1" color="white" size="sm" />
-            <Text color="white" fontSize="12">
-              Home
-            </Text>
-          </Center>
-        </Pressable>
-        <Pressable
-          cursor="pointer"
-          opacity={selected === 1 ? 1 : 0.5}
-          py="2"
-          flex={1}
-          onPress={() => setSelected(1)}>
-          <Center>
-            <CheckIcon mb="1" color="white" size="sm" />
-            <Text color="white" fontSize="12">
-              Search
-            </Text>
-          </Center>
-        </Pressable>
-        <Pressable
-          cursor="pointer"
-          opacity={selected === 2 ? 1 : 0.6}
-          py="2"
-          flex={1}
-          onPress={() => setSelected(2)}>
-          <Center>
-            <CheckIcon mb="1" color="white" size="sm" />
-            <Text color="white" fontSize="12">
-              Cart
-            </Text>
-          </Center>
-        </Pressable>
-        <Pressable
-          cursor="pointer"
-          opacity={selected === 3 ? 1 : 0.5}
-          py="2"
-          flex={1}
-          onPress={() => setSelected(3)}>
-          <Center>
-            <CheckIcon mb="1" color="white" size="sm" />
-            <Text color="white" fontSize="12">
-              Account
-            </Text>
-          </Center>
-        </Pressable>
+      <HStack bg="#fff" alignItems="center" safeAreaBottom shadow={6}>
+        {tabbar.map((i, index) => (
+          <Pressable
+            cursor="pointer"
+            opacity={selected === index ? 1 : 0.5}
+            py="3"
+            flex={1}
+            onPress={() => setSelected(index)}>
+            <Center>
+              <Icon name={i.icon} size={30} color="#4F8EF7" />
+              <Text color="#000" fontSize="12">
+                {i.name}
+              </Text>
+            </Center>
+          </Pressable>
+        ))}
       </HStack>
     </Box>
   )
